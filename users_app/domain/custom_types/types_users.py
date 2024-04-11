675ae97e726d2f4a7c9d3e9d1a4f3a30af1c584f
zus666 <t4g1.86@gmail.com>
2024-04-11 09:49:07 +0500

    change uidtype

--- users_app/domain/custom_types/types_users.py
+++ users_app/domain/custom_types/types_users.py
@@ -1,4 +1,12 @@
-from typing import TypeAlias
+from typing import Any
+
+from pydantic_core import CoreSchema, core_schema
+from pydantic import GetCoreSchemaHandler
 
 
-UIDType: TypeAlias = int
+class UIDType(int):
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> CoreSchema:
+        return core_schema.no_info_after_validator_function(cls, handler(int))
