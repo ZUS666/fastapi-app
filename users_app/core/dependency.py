fb282bdb51607012f34250c0c8f5e2dc3963454d
zus666 <t4g1.86@gmail.com>
2024-04-11 09:48:35 +0500

    add typing

--- users_app/core/dependency.py
+++ users_app/core/dependency.py
@@ -5,17 +5,18 @@ from punq import Container
 
 
 class Impl:
-    _instances: set[Self] = {}
+    _instances: dict[Self, Self] = {}
 
     def __init__(self) -> None:
         self.container: Container = Container()
 
     def __call__(self, *args, **kwargs) -> Self:
         if self not in self._instances:
-            self._instances.add(self)
-        return self._instances[0]
+            instance = super().__call__(*args, **kwargs)
+            self._instances[self] = instance
+        return self._instances[self]
 
-    def register_all(self, objs: tuple[tuple[Any, Any], ...]) -> None:
+    def register_all(self, objs: tuple[tuple[type[Any], type[Any]], ...]) -> None:
         for obj in objs:
             self.container.register(*obj)
 
