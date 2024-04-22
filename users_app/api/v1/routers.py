40fdde294c14f48c059b9409823d202b75fbcec3
zus666 <t4g1.86@gmail.com>
2024-04-22 08:10:52 +0500

    include routers by func

--- users_app/api/v1/routers.py
+++ users_app/api/v1/routers.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 
 from .auth_routers import auth_router
+from .avatar_routers import avatar_router
 from .user_ruoters import user_router
 
 
-router_v1 = APIRouter(prefix='/api/v1')
-
-router_v1.include_router(auth_router)
-router_v1.include_router(user_router)
+def get_routers_v1() -> APIRouter:
+    router_v1 = APIRouter(prefix='/api/v1')
+    router_v1.include_router(auth_router)
+    router_v1.include_router(user_router)
+    router_v1.include_router(avatar_router)
+    return router_v1
